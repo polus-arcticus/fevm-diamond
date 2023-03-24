e890854278559948459ba2a6897152837af18bdf
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-03-24 14:59:20 -0700

    example make and get deal

--- hardhat/test/dealProposal.js
+++ hardhat/test/dealProposal.js
@@ -1,8 +1,9 @@
 /* global ethers describe before it */
-
 const { deployDiamond } = require('../scripts/deploy.js')
-
+const exampleFile = require('./lighthouse.storage.json')
+const CID = require('cids')
 async function getHelia () {
+  const { base16 } = await import('multiformats/bases/base16')
   const { createHelia } = await import('helia')
   const { createLibp2p } = await import('libp2p')
   const { noise } = await import('@chainsafe/libp2p-noise')
@@ -46,18 +47,21 @@ async function getHelia () {
 
   const fs = unixfs(helia)
 
-  return { helia, fs }
+  return { CID, helia, fs, base16 }
 }
 
 
 describe('Deal Proposal', async (accounts) => {
+  
   let diamondAddress
   let simpleCoinAddress
   let dealClientFacet
   let heliaInstance
   let fs
+  let CID
+  let base16
   before(async () => {
-    ({ helia: heliaInstance, fs } = await getHelia());
+    ({ helia: heliaInstance, fs, CID, base16 } = await getHelia());
 
     ({ diamond: diamondAddress, simpleCoin: simpleCoinAddress } = await deployDiamond())
     dealClientFacet = await ethers.getContractAt('DealClientFacet', diamondAddress)
@@ -65,6 +69,42 @@ describe('Deal Proposal', async (accounts) => {
   })
 
   it('should be able to make a deal proposal', async () => {
+    // parse cid
+    const cidHex = '0x' + new CID(exampleFile.piece_CID).toString('base16').substring(1)
+
+    const verified = (true).toString()
+    const skipIpniAnnounce = (true).toString()
+    const removeUnsealedCopy = (true).toString()
+
+    const extraParamsV1 = [
+      exampleFile.car_Link,
+      exampleFile.car_Size,
+      skipIpniAnnounce,
+      removeUnsealedCopy
+    ]
+
+    const dealRequestStruct = [
+      cidHex,
+      exampleFile.piece_Size,
+      verified,
+      exampleFile.piece_CID,
+      520000,
+      1555200,
+      0, // storage price per epoch
+      0, // provider collateral
+      0, // client collateral
+      1, // extraparams version
+      extraParamsV1
+    ]
+
+    const tx = await dealClientFacet.makeDealProposal(dealRequestStruct)
+    const receipt = await tx.wait()
+
+    console.log(receipt.events[0])
+
+    const deal = await dealClientFacet.getDealProposal(receipt.events[0].topics[1])
+    console.log(deal)
+
 
   })
 })
